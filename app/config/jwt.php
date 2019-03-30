897b0c708e9d597552c279c9464e963a4d575bd4
ROCKHAFELLA\phalconVEE <ugo@fint.ng>
2019-03-30 15:29:12 +0100

    my first commit

---
+++ app/config/jwt.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Intellisense.
+ * Engineer: Henry Ugochukwu
+ * Email: phalcon.vee@gmail.com
+ * Website: intellisense.com.ng
+ * Date: 24/03/2019
+ * Time: 6:23 PM
+ */
+
+$config['jwt_token_key'] = 'Y*VMeK5h6UyWP4J=.DVUCq?WjKWcu}';
