af5663f97c69eabc4fefb54d90ed3fbf64c2265c
Henry Ugochukwu <phalcon.vee@gmail.com>
2019-04-08 15:52:42 +0100

    Update email.php

--- app/config/email.php
+++ app/config/email.php
@@ -6,67 +6,6 @@
  * Time: 2:38 PM
  */
 
-/*$config['protocol']         = 'mail';                 // 'mail', 'sendmail', or 'smtp'
-$config['smtp_host']        = 'mail.fint.ng';
-$config['smtp_port']        = 465;
-$config['smtp_user']        = 'ugo@fint.ng';
-$config['smtp_pass']        = 'Ugochukwu1000';
-$config['charset']          = 'utf-8';
-$config['wordwrap']         = TRUE;
-$config['mailtype']         = 'html'; */
-
-/*
-
-// SMTP
-$config['smtp_host']        = "ssl://smtp.googlemail.com";
-$config['smtp_port']        = 465;
-$config['smtp_user']        = "ugostein1000@gmail.com";
-$config['smtp_pass']        = "Ugochukwu1000";
-$config['charset']          = "utf-8";
-$config['smtp_timeout']     = 30;                        // (in seconds)
-$config['smtp_crypto']      = "ssl";
-$config['wordwrap']         = TRUE;
-$config['mailtype']         = "html";
-$config['crlf']             = "\r\n",
-$config['newline']          = "\r\n";
-
-*/
-
-/*
-// PHP Mailer
-$config['useragent']        = 'PHPMailer';
-$config['protocol']         = 'smtp';
-$config['mailpath']         = '/usr/sbin/sendmail';
-$config['smtp_host']        = 'smtp.gmail.com';
-$config['smtp_user']        = 'ugostein1000@gmail.com';
-$config['smtp_pass']        = 'Ugochukwu1000';
-$config['smtp_port']        = 465;
-$config['smtp_timeout']     = 30;                        // (in seconds)
-$config['smtp_crypto']      = 'ssl';                    // '' or 'tls' or 'ssl'
-$config['wordwrap']         = true;
-$config['wrapchars']        = 76;
-$config['mailtype']         = 'html';                   // 'text' or 'html'
-$config['charset']          = 'iso-8859-1';
-$config['validate']         = true;
-$config['priority']         = 3;                        // 1, 2, 3, 4, 5
-$config['crlf']             = "\n";                     // "\r\n" or "\n" or "\r"
-$config['newline']          = "\n";                     // "\r\n" or "\n" or "\r"
-$config['bcc_batch_mode']   = false;
-$config['bcc_batch_size']   = 200;
-*/
-
-// Send Grid
-
-/*$config['protocol']         = 'smtp';
-$config['smtp_host']        = 'smtp.sendgrid.net';
-$config['smtp_port']        = 587;
-$config['smtp_user']        = 'nnamdi.okeke@kraneum.com';
-$config['smtp_pass']        = 'krAFRICA00';
-$config['crlf']             = "\r\n";
-$config['newline']          = "\r\n";
-$config['mailtype']         = 'html';*/
-
-
 // Mail Trap
 $config['protocol']         = 'smtp';
 $config['smtp_host']        = 'smtp.mailtrap.io';
